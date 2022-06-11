65114ca6855ac85034b75fc15f577787a30c2cf9
Tristan Guest <tristanguest@gmail.com>
2022-06-11 15:12:59 -0300

    Fetch data from Github graphql api

---
+++ models.ts
@@ -0,0 +1,37 @@
+export type DocumentHead = {
+  title: string;
+  description: string;
+};
+
+export interface Repo {
+  [key: string]: RepoFields;
+}
+
+export interface RepoFields {
+  id: string;
+  description: string;
+  forkCount: number;
+  name: string;
+  nameWithOwner: string;
+  stargazerCount: number;
+  issues: {
+    totalCount: number;
+  };
+  mentionableUsers: {
+    totalCount: number;
+  };
+  assignableUsers: {
+    totalCount: number;
+  };
+  pullRequests: {
+    totalCount: number;
+  };
+  object: {
+    __typename: string;
+    id: string;
+    history: {
+      __typename: string;
+      totalCount: number;
+    };
+  };
+}
