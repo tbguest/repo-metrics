ea4fbdab5fd6a6181a2ea39eb01eb2d0f270c95c
Tristan Guest <tristanguest@gmail.com>
2022-06-25 13:59:44 -0300

    Refine responsive layout

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Made by&nbsp;
       <a href="https://github.com/tbguest/">@tbguest</a>
     </footer>
   );
