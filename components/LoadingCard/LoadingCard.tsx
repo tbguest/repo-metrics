8cd978604d9be397a83c4198fa0b722cb3cd9930
Tristan Guest <tristanguest@gmail.com>
2022-10-29 14:35:45 -0300

    Translate loading card

---
+++ components/LoadingCard/LoadingCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingCard = () => {
+  return (
+    <div className="border-2 border-[color:var(--grey)] rounded pt-8 pr-4 pb-4 pl-4 md:pr-8 md:pl-8 h-80 mb-12 animate-pulse bg-[color:var(--background-color-shimmer)]"></div>
+  );
+};
+
+export { LoadingCard };
--- components/PlaceholderCard/PlaceholderCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./PlaceholderCard.module.css";
-
-const PlaceholderCard = () => {
-  return <div className={styles.card}></div>;
-};
-
-export { PlaceholderCard };
