11d17433c99b5080dcf8a4b2c04cb8018b57f4d3
Tristan Guest <tristanguest@gmail.com>
2022-10-29 13:30:44 -0300

    Delete ununsed layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Layout } from "../components/Layout";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   // function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      {/* <Layout> */}
       <Component {...pageProps} />
-      {/* </Layout> */}
     </SessionProvider>
   );
 }
